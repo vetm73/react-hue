a40db3538683a2c478faf73d9fb26b65604ab9c3
Mike van der Vet <mike@seventythree.cc>
2016-10-29 13:27:47 +0200

    Merge branch 'master' of /Users/mike/Sites/react-hue with conflicts.

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,20 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-// var express    = require('express');        // call express
 import express from 'express';
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var fs         = require('fs');
+import bodyParser from 'body-parser';
+import fs from 'fs';
 
-var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const app = express();                 // define our app using express
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+const port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+const router = express.Router();              // get an instance of the express Router
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -39,12 +32,12 @@ router.get('/', function(req, res) {
 router.route('/lights')
 
 // create a bear (accessed at POST http://localhost:8080/api/bears)
-  .post(function(req, res) {
+  .post((req, res) => {
 
   })
 
   // get all the bears (accessed at GET http://localhost:8080/api/bears)
-  .get(function(req, res) {
+  .get((req, res) => {
       // res.setHeader('Content-Type', 'application/json');
       res.json(config);
   });
@@ -52,8 +45,8 @@ router.route('/lights')
 router.route('/lights/:light_id')
 
 // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-  .get(function(req, res) {
-      var light_id = req.params.light_id;
+  .get((req, res) => {
+      const light_id = req.params.light_id;
       if(!config[light_id]) {
           res.json({error: 'Light ' + light_id + ' not found'});
       }
@@ -61,8 +54,8 @@ router.route('/lights/:light_id')
   })
 
   // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-  .put(function(req, res) {
-      var light_id = req.params.light_id;
+  .put((req, res) => {
+      const light_id = req.params.light_id;
 
       if(!config[light_id]) {
         res.json({error: 'Light ' + light_id + ' not found'});
@@ -70,7 +63,6 @@ router.route('/lights/:light_id')
 
       if(req.body.on) {
           config[light_id]['state']['on'] = req.body.on;
-          // res.json({ message: 'Light ' + light_id + ' updated!' });
           res.json(config[light_id]);
       }
   });
