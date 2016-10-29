6aac9775e6848724ecae0215e12c5497ee5aff61
Mike van der Vet <mike@seventythree.cc>
2016-10-29 10:46:11 +0200

    server ES6

--- web/Container.js
+++ web/Container.js
@@ -1,6 +1,10 @@
 /**
 <<<<<<< HEAD
+<<<<<<< HEAD
  * Created by mike on 29/10/2016.
+=======
+ * Created by mike on 28/10/2016.
+>>>>>>> bc3a83f... server ES6
 =======
  * Created by mike on 28/10/2016.
 >>>>>>> bc3a83f... server ES6
